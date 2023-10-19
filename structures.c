17ce899f1314b89057a94ed7add0e748f8881709
Kaushigithub <105336807+Kaushigithub@users.noreply.github.com>
2023-10-20 01:37:54 +0530

    Add files via upload

--- structures.c
+++ structures.c
@@ -16,6 +16,7 @@ struct FacultyDetail
 	char name[50];
 	char password[50];
     char department[50];
+    char designation[50];
     char email[50];
     char address[50];
 };
@@ -28,4 +29,14 @@ struct CourseDetail
     char dept[50];
     int no_seats;
     int course_credits;
+    int max_seats;
+};
+
+struct EnrolledCourse
+{
+	int student_id;
+	int course_id;
+	char course_name[50];
+	int fac_id;
+    int enrolled;
 };
